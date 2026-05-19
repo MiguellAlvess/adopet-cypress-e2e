6131bb3513301a2b89c9baaa6b7df5df21e1e1e6
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-05-19 11:50:22 -0300

    test: add automated tests for user login error scenarios

--- cypress/e2e/login-page.cy.js
+++ cypress/e2e/login-page.cy.js
@@ -11,4 +11,18 @@ describe("Login page", () => {
     cy.get('[data-test="input-loginPassword"]').type("Me12345678");
     cy.get('[data-test="submit-button"]').click();
   });
+  it("should display an error when trying to login a user without filling in the email field", () => {
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="input-loginPassword"]').type("Me12345678");
+    cy.get('[data-test="submit-button"]').click();
+    cy.contains("É necessário informar um endereço de email");
+  });
+  it("should display an error when trying to login a user without filling in the password field", () => {
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="input-loginEmail"]').type(
+      "miguelteste983213@gmail.com",
+    );
+    cy.get('[data-test="submit-button"]').click();
+    cy.contains("Insira sua senha");
+  });
 });
