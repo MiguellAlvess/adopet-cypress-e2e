fa9343f6e19088942201c3b63c4b9750f6846dd6
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-05-19 11:12:01 -0300

    test: ensure user can be registred

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Adopet", () => {
+  it("should register a user successfully", () => {
     cy.visit("https://adopet-frontend-cypress.vercel.app/");
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type("Miguel Alves");
+    cy.get('[data-test="input-email"]').type("miguelteste983213@gmail.com");
+    cy.get('[data-test="input-password"]').type("Me12345678");
+    cy.get('[data-test="input-confirm-password"]').type("Me12345678");
+    cy.get('[data-test="submit-button"]').click();
   });
 });
