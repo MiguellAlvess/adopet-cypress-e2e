4b9efc10a7795c15d0aeb2bb95c81d7112efe4b1
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-05-19 13:59:29 -0300

    feat: add projectid to integration with cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  projectId: "tyefh8",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
